9e4cfc074416b3c0b093ba7150075e2561607746
SonnyC56 <cbcirasu@syr.edu>
2023-03-26 02:14:48 -0400

    added some conditions

--- main.js
+++ main.js
@@ -3,17 +3,31 @@ const WebSocketServer = require('ws');
  
 // Creating a new websocket server
 const wss = new WebSocketServer.Server({ port: 8080 })
+
+let connectedUserID = null;
+let numberOfClients = 0;
  
 // Creating connection using websocket
 wss.on("connection", ws => {
-    console.log("new client connected");
+    numberOfClients++;
+    console.log("number of clients connected since server start", numberOfClients);
  
     // sending message to client
     ws.send('Welcome, you are connected!');
  
     //on message from client
     ws.on("message", data => {
-        console.log(`Client has sent us: ${data}`)
+      //  console.log(`Client has sent us: ${data}`)
+        data = JSON.parse(data)
+   //     console.log('data.uuid: ', data.uuid)
+
+        if(connectedUserID == null && data.uuid){
+            connectedUserID = data.uuid
+        }
+        if (data.uuid == connectedUserID && connectedUserID){
+            console.log('data.uuid == connectedUserID', connectedUserID)
+       //   console.log(`Client has sent us: ${data}`)
+        }
     });
  
     // handling what to do when clients disconnects from server
