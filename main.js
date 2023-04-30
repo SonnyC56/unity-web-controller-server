1bc74f188c2dc139cd41c5a2930155c82f4eeae9
SonnyC56 <cbcirasu@syr.edu>
2023-04-30 03:49:19 -0400

    added more queing

--- main.js
+++ main.js
@@ -1,56 +1,97 @@
-
 const WebSocketServer = require('ws');
-const wss = new WebSocketServer.Server({ port: 8080 });
+const wss = new WebSocketServer.Server({ port: 8090 });
 
 let connectedClients = [];
 let controlQueue = [];
 let unityClient = null;
+let websocketInControl = null;
 
 wss.on("connection", ws => {
-    console.log('client connected to server')
-    connectedClients.push(ws);
+    connectedClients.push(ws);  
+    console.log('client connected to server. Connected Clients: ', connectedClients.length)
 
-    ws.on("message", data => {
-        data = JSON.parse(data);
-  
-   //     console.log('recieving data from client: ', data)
-        if (data.type === "unity") {
-            // This is the Unity client
-            unityClient = ws;
-            console.log('UNITY CONNECTED :', unityClient)
-        } else if (data.type === "done") {
-            // The current client is done controlling the camera
-            controlQueue.shift();
+  ws.on("message", data => {
+    data = JSON.parse(data);
 
-            // Allow the next client in line to take control
-            if (controlQueue.length > 0) {
-                controlQueue[0].send(JSON.stringify({ type: "control" }));
-            }
-        }   
-      if (unityClient) {
-         unityClient.send(JSON.stringify(data));
-         console.log('sending data to unity: ', data)
-      } 
-        
-    });
-
-    ws.on("close", () => {
-        // Remove the disconnected client from the connected clients list and control queue
-        connectedClients = connectedClients.filter(client => client !== ws);
-        controlQueue = controlQueue.filter(client => client !== ws);
-
-        // If this was the Unity client, set it to null
-        if (ws === unityClient) {
-            unityClient = null;
-            console.log('UNITY DISCONNECTED')
-        }
-    });
-
-    // Add the new client to the end of the control queue
-    controlQueue.push(ws);
-
-    // If this is the only client in the queue, allow them to take control immediately
-    if (controlQueue.length === 1) {
-        ws.send(JSON.stringify({ type: "control" }));
+    if (data.type === "unity") {
+      // This is the Unity client
+      unityClient = ws;
+      console.log('UNITY CONNECTED :', unityClient)
     }
+
+     if (data.type === "done") {
+      // The current client is done controlling the camera
+      controlQueue.shift();
+      // Allow the next client in line to take control
+      websocketInControl = controlQueue[0];
+      if(websocketInControl){
+      websocketInControl.send(JSON.stringify({ type: "control" }));
+      }
+
+    } 
+
+    if (data.type === "join") {
+      // Add the new client to the end of the control queue
+      controlQueue.push(ws);
+      console.log("adding to control que")
+      websocketInControl = controlQueue[0];
+      controlQueue[0].send(JSON.stringify({ type: "control" }));
+      // Notify all clients of their new position in the control queue
+      connectedClients.forEach(client => {
+        if (client !== unityClient) {
+          const position = controlQueue.indexOf(client);
+          const totalClients = connectedClients.length
+          client.send(JSON.stringify({  type: "queue", position: position, totalClients: totalClients }));
+        }
+      });
+    }
+    
+    if (unityClient) {
+      unityClient.send(JSON.stringify(data));
+      console.log('sending data to unity: ', data)
+    }
+  });
+
+  ws.on("close", () => {
+    console.log('client disconnected')
+  
+    // Remove the disconnected client from the connected clients list and control queue\
+    const connectedClientsIndex = connectedClients.indexOf(ws);
+    const controlQueueIndex = controlQueue.indexOf(ws);
+    if (connectedClientsIndex > -1) { // only splice array when item is found
+        connectedClients.splice(connectedClientsIndex, 1); // 2nd parameter means remove one item only
+      }
+      if (controlQueueIndex > -1) { // only splice array when item is found
+        controlQueue.splice(controlQueueIndex, 1); // 2nd parameter means remove one item only
+      }
+
+    
+    // If the disconnected client was in control, assign control to the next user in the queue
+    if (ws === websocketInControl) {
+      controlQueue.shift();
+      if (controlQueue.length > 0) {
+        websocketInControl = controlQueue[0];
+        websocketInControl.send(JSON.stringify({ type: "control" }));
+      } else {
+        websocketInControl = null;
+      }
+    }
+    
+    // If this was the Unity client, set it to null
+    if (ws === unityClient) {
+      unityClient = null;
+      console.log('UNITY DISCONNECTED')
+    }
+  
+    // Notify all clients of their new position in the control queue
+    connectedClients.forEach(client => {
+      if (client !== unityClient) {
+        const position = controlQueue.indexOf(client);
+        const totalClients = connectedClients.length
+        client.send(JSON.stringify({  type: "queue", position: position, totalClients: totalClients }));
+      }
+    });
+  });
+  
 });
+
